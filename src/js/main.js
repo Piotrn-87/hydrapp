acac4ad474bd1be62c3274a7eb9329a8097fabc8
Piotr Napieralski <piotrn-87@o2.pl>
2019-07-22 20:49:10 +0200

    Feat: Added js to app

--- src/js/main.js
+++ src/js/main.js
@@ -3,20 +3,38 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
   });
 }
 
-// place your code below
+const buttonAdd = document.querySelector(".button-add--js");
+const buttonRemove = document.querySelector(".button-remove--js");
+const value = document.querySelector(".container__number--js");
+const key = new Date().toISOString().slice(0, 10);
 
+if (!localStorage.getItem(key)) {
+  localStorage.setItem(key, 0)
+  value.innerHTML = "0";
+} else {
+  value.innerHTML = localStorage.getItem(key);
+}
 
-console.log(`Hello world!`);
+buttonAdd.addEventListener('click', (e) => {
+  localStorage.setItem(key, parseInt(localStorage.getItem(key)) + 1)
+  value.innerHTML = localStorage.getItem(key);
+  // value.innerHTML = parseInt(value.innerHTML) + 1;
+});
 
-
+buttonRemove.addEventListener('click', (e) => {
+  const currentValue = parseInt(value.innerHTML);
+  if (currentValue > 0) {
+    value.innerHTML = parseInt(value.innerHTML) - 1;
+  }
+});
